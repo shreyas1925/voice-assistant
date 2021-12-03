20de9f3367902802e7b4e3db609e3dd273e9b7d2
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-12-04 02:24:53 +0530

    dealing with UI Components

--- Components/MainComponent.js
+++ Components/MainComponent.js
@@ -2,14 +2,19 @@ export function MainComponent() {
   return (
     <div className="main_window">
       <div className="window">
-        <div className="text"></div>
-
+        <div className="text text-center">
+          <h2 className="mt-4">VOICE COORDINATOR</h2>
+          <p className="text-center mt-3">
+            You can record the speech of others while they are speaking and you
+            can keep track of them through this app along with your voice
+          </p>
+        </div>
         <div className="info">
           <div className="row">
             <div className="col-lg-6 col-md-6 col-12">
               <div className="dictate_info">
                 <img src="" />
-                <h3></h3>
+                <h3>Speak normally but</h3>
               </div>
             </div>
             <div className="col-lg-6 col-md-6 col-12">
