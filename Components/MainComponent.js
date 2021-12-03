ecca6384411dca91aa29b68719318e38ddc346ec
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-12-04 01:45:04 +0530

    setting up the new project

---
+++ Components/MainComponent.js
@@ -0,0 +1,26 @@
+export function MainComponent() {
+  return (
+    <div className="main_window">
+      <div className="window">
+        <div className="text"></div>
+
+        <div className="info">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="dictate_info">
+                <img src="" />
+                <h3></h3>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="listen_info">
+                <img src="" />
+                <h3></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
