16b80a8c040ef495a7cc5f5276e23f1a5b42a351
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-12-04 02:46:03 +0530

    dealing with UI Components

--- Components/MainComponent.js
+++ Components/MainComponent.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export function MainComponent() {
   return (
     <div className="main_window">
@@ -13,14 +14,20 @@ export function MainComponent() {
           <div className="row">
             <div className="col-lg-6 col-md-6 col-12">
               <div className="dictate_info">
-                <img src="" />
-                <h3>Speak normally but</h3>
+                <Image src={} />
+                <p>
+                  Speak normally but make sure your voice is clear and free of
+                  hesitation muffling
+                </p>
               </div>
             </div>
             <div className="col-lg-6 col-md-6 col-12">
               <div className="listen_info">
-                <img src="" />
-                <h3></h3>
+                <Image src={} />
+                <p>
+                  Get comfortable and make sure that your microphone is working
+                  correctly
+                </p>
               </div>
             </div>
           </div>
